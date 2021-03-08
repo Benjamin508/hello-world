7f9b18ffd424140840b1bd3155bd38e7cf5499a4
Benjamin McSwain <benjamin.mcswain88@gmail.com>
2021-03-08 12:44:44 -0500

    descriptive
    changed to add name and age

---
+++ hello world.java
@@ -0,0 +1,18 @@
+public class Helloworld
+{
+public static void main(String[]args)
+{
+system.out.println("Hello World");
+
+//name is a reference variable
+ String name = "George Washington";
+ 
+ System.out.println(name);
+ 
+ //age is a primative variable
+ int age = 24;
+
+System.out.println(age);
+
+}//end main
+}// end class
